81cc8eac5d0c88bed62aa6c2a1272cb60abaf011
ibobal <119145325+ibobal@users.noreply.github.com>
2025-03-22 18:46:33 +0300

    added routes and corresponding components

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import workintech from "/workintech.svg";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <img src={workintech} alt="workintech" />
+    </div>
+  );
+}
---
+++ src/components/Success.jsx
@@ -0,0 +1,10 @@
+import workintech from "/workintech.svg";
+
+export default function Success() {
+  return (
+    <div>
+      <h1>Success</h1>
+      <img src={workintech} alt="workintech" />
+    </div>
+  );
+}
