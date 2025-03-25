79de8c5e8135ae19de69af4640dd2fb0a41d191a
ibobal <119145325+ibobal@users.noreply.github.com>
2025-03-25 17:35:53 +0300

    completed success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,10 +1,16 @@
-import workintech from "/workintech.svg";
+import { Link } from "react-router-dom";
+import logo from "/images/iteration-1-images/logo.svg";
 
 export default function Success() {
   return (
-    <div>
-      <h1>Success</h1>
-      <img src={workintech} alt="workintech" />
+    <div className="bg-[#CE2829] py-10 w-screen h-screen bg-cover bg-center flex flex-col place-items-center">
+      <Link to="/">
+        <img src={logo} alt="logo" className="py-5" />
+      </Link>
+      <p className="font-[Barlow] font-light text-7xl text-white text-center flex flex-col flex-1 justify-center gap-8">
+        <p>TEBRİKLER!</p>
+        <p>SİPARİŞİNİZ ALINDI!</p>
+      </p>
     </div>
   );
 }
