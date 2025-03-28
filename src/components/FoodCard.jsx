af9ef7f8a134885fddb1d59294a0a535aa12ff77
ibobal <119145325+ibobal@users.noreply.github.com>
2025-03-28 07:11:24 +0300

    Update FoodCard.jsx

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -3,18 +3,18 @@ export default function FoodCard(props) {
   return (
     <div
       key={pizza.id}
-      className="bg-white shadow-md rounded-lg p-5 w-80 flex-shrink-0"
+      className="bg-white shadow-md rounded-lg p-5 w-80 flex-shrink-0 font-[Barlow]"
     >
       <img
         src={pizza.image}
         alt={pizza.name}
         className="w-full h-40 object-contain rounded-md mb-4"
       />
-      <h4 className="text-2xl font-medium mb-2">{pizza.name}</h4>
-      <div className="text-md font-semibold text-[#CE2829] flex justify-between">
-        <span>{pizza.rating}</span>
-        <span>{pizza.usersRated}</span>
-        <span>{pizza.price} ₺</span>
+      <h4 className="text-2xl font-semibold mb-2">{pizza.name}</h4>
+      <div className="flex justify-between">
+        <span className="text-[#5F5F5F] text-md">{pizza.rating}</span>
+        <span className="text-[#5F5F5F] text-md">({pizza.usersRated})</span>
+        <span className="text-[#292929] font-bold text-lg">{pizza.price} ₺</span>
       </div>
     </div>
   );
