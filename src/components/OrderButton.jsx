58758fb7a72d1e4e185d21d36ec45a9f1cc4193e
ibobal <119145325+ibobal@users.noreply.github.com>
2025-03-28 09:15:11 +0300

    Update OrderButton.jsx

--- src/components/OrderButton.jsx
+++ src/components/OrderButton.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function OrderButton() {
   return (
     <Link
-      to="/order"
-      className="hover:bg-gray-50 bg-white text-[#CE2829] text-lg font-medium py-3 px-10 my-15 rounded-3xl shadow-lg transition duration-300 ease-in-out transform"
+      to="#food-slider"
+      className="bg-white text-[#CE2829] text-lg font-medium py-3 px-10 my-15 rounded-3xl shadow-lg hover:bg-gray-100 transition duration-200 ease-in-out"
     >
       SİPARİŞ VER
     </Link>
